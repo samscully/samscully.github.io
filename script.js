241b3bf7b16bd0838e22d489921236818a45934d
Sam Scully <60153073+samscully@users.noreply.github.com>
2020-05-02 13:02:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+(function(){
+    var words = [
+        ' designer',
+        ' painter',
+        ' creator',
+        
+        ], i = 0;
+    setInterval(function(){
+        $('#changingword').fadeOut(function(){
+            $(this).html(words[i=(i+1)%words.length]).fadeIn();
+        });
+    }, 3000);
+})();
+
